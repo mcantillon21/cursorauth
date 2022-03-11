695d0df0349748d3b86cc124626b945935d63d7d
mcan <mcan@stanford.edu>
2022-03-11 15:33:52 -0800

    random button placing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
 import logo from './logo.svg';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import CustomBtn from './components/CustomBtn';
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main:"#2e1667",
+    },
+    secondary: {
+      main:"#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ThemeProvider theme={theme}>
+        <h2
+          style={{
+            position: "absolute",
+            left: `${(Math.random()* 600)}px`,
+            top: `${(Math.random()* 750)}px`,
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <CustomBtn txt="Button"
+            onClick={() => this.handleClick(1)}
+          />
+        </h2>
+        <h2
+          style={{
+            position: "absolute",
+            left: `${(Math.random()* 600)}px`,
+            top: `${(Math.random()* 750)}px`,
+          }}
+        >
+          <CustomBtn txt="Button"/>
+        </h2>
+        <h2
+          style={{
+            position: "absolute",
+            left: `${(Math.random()* 600)}px`,
+            top: `${(Math.random()* 750)}px`,
+          }}
+        >
+          <CustomBtn txt="Button"/>
+        </h2>
+        <h2
+          style={{
+            position: "absolute",
+            left: `${(Math.random()* 600)}px`,
+            top: `${(Math.random()* 750)}px`,
+          }}
+        >
+          <CustomBtn txt="Button"/>
+        </h2>
+        <h2
+          style={{
+            position: "absolute",
+            left: `${(Math.random()* 600)}px`,
+            top: `${(Math.random()* 750)}px`,
+          }}
+        >
+          <CustomBtn txt="Button"/>
+        </h2>
+        <h2
+          style={{
+            position: "absolute",
+            left: `${(Math.random()* 600)}px`,
+            top: `${(Math.random()* 750)}px`,
+          }}
+        >
+          <CustomBtn txt="Button"/>
+        </h2>
+      </ThemeProvider>
     </div>
   );
 }
